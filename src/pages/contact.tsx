20c882ef097a3a824ee1b468e697333781985662
kanwarpalmanu <kanwarpalmanu@users.noreply.github.com>
2020-05-25 13:53:10 +0100

    Edited pages

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 
 const SecondPage = (props: PageProps) => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2 ({props.path})</p>
+    <SEO title="Contact us" />
+    <h1>Contact us</h1>
+    <p>Email us at admin@themindfulway.org({props.path})</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
