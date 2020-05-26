7d99043c77f692cfb9a407d4a0742964b294a3f6
kanwarpalmanu <kanwarpalmanu@users.noreply.github.com>
2020-05-27 00:15:50 +0100

    small change for testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>Mindfulness is a way of life, and the principles of mindfulness can be applied to each and every living moment.</p>
+    <p>Mindfulness is a way of life and the principles of mindfulness can be applied to each and every living moment.</p>
     <p>This website is dedicated to sharing knowledge, experiences and thoughts about living life <i>The Mindful Way</i></p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
