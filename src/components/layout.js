9e986c4d4f7e007ebbf3a5a4572c8446e16588ef
Manu Kanwarpal <kanwarpalmanu@hotmail.com>
2020-06-08 08:59:43 +0100

    removed maxwidth

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,7 +30,6 @@ const Layout = ({ children }) => {
         style={{
           textAlign:'left',
           margin: `0 auto`,
-          maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >       
